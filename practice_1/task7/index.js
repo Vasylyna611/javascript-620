94f0e9e929005db56092dc030e8cec276489d0da
Vasylyna611 <vasylynakuzyshyn@gmail.com>
2021-08-02 20:10:27 +0300

    Add tasks

---
+++ practice_1/task7/index.js
@@ -0,0 +1,4 @@
+let userLogin = prompt("Enter your login"); 
+let userEmail = prompt("Enter your email");
+let userPassword = prompt("Enter your password");
+alert('Dear ' + userLogin + ' your email is ' + userEmail + ' your password is ' + userPassword);
---
+++ practice_1/task8/index.js
@@ -0,0 +1,8 @@
+function dateCalc() {
+	let secInHour = 60 * 60;
+	let secInDay = secInHour * 24;
+	let secInMonth = secInDay * 30;
+	console.log("Seconds in hour " + secInHour);
+	console.log("Seconds in day " + secInDay);
+	console.log("Seconds in month " + secInMonth);
+}
